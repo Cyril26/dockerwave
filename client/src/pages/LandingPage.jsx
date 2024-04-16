9f5040b18e0f97ff976108b5c20823e8dad1dd51
Cyril26 <cyrilodm26@gmaoll.com>
2024-04-16 20:49:03 +0000

    landing page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,24 +1,58 @@
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import wave from "../assets/wave.png";
+import docker from "../assets/docker.png";
+import pull from "../assets/pull.png";
+import push from "../assets/push.png";
+import view from "../assets/view.png";
+import dash from "../assets/dash.png";
 
 export const LandingPage = () => {
+  const [selectedButton, setSelectedButton] = useState("deploy");
+
+  const handleButtonClick = (buttonId) => {
+    setSelectedButton(buttonId);
+  };
   return (
     <>
       <header>
-        <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+        <nav className="bg-white border-gray-200 px-4 lg:px-6  dark:bg-gray-800">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="#" className="flex items-center">
-              <svg
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                className="fill-current text-white h-8 w-8 mr-2"
-              >
-                <path d="M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z" />
-              </svg>
+              <img src={wave} alt="wave" className="h-20 w-20 mr-2" />
               <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
                 DockerWave
               </span>
             </a>
+
+            <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1">
+              <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                <li>
+                  <a
+                    href="#about"
+                    className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  >
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#features"
+                    className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  >
+                    Features
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#contact"
+                    className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  >
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </div>
             <div className="flex items-center lg:order-2">
               <Link
                 to={"/login"}
@@ -27,39 +61,12 @@ export const LandingPage = () => {
                 Login
               </Link>
 
-              <button
-                data-collapse-toggle="mobile-menu-2"
-                type="button"
-                className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                aria-controls="mobile-menu-2"
-                aria-expanded="false"
+              <Link
+                to={"/register"}
+                className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
               >
-                <span className="sr-only">Open main menu</span>
-                <svg
-                  className="w-6 h-6"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <svg
-                  className="hidden w-6 h-6"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-              </button>
+                Signup
+              </Link>
             </div>
           </div>
         </nav>
@@ -76,7 +83,7 @@ export const LandingPage = () => {
             </p>
             <Link
               to="/register"
-              className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+              className=" inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-2xl bg-blue-500 hover:bg-blue-300 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
             >
               Get started
               <svg
@@ -94,15 +101,131 @@ export const LandingPage = () => {
             </Link>
           </div>
           <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-            <img
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png"
-              alt="mockup"
-            />
+            <img src={docker} alt="docker" />
           </div>
         </div>
       </section>
 
-      <section className="bg-gray-50 dark:bg-gray-800">
+      <section className="bg-gray-50 dark:bg-gray-800" id="features">
+        <div className="flex flex-col items-center justify-center w-2/3 mx-auto text-center px-4 py-8">
+          <h1 className="mb-4 text-4xl font-extrabold text-gray-900 dark:text-white">
+            Accelerate how you build, share, and run applications
+          </h1>
+          <p className="text-gray-500 my-5">
+            DockerWave helps developers build, push, pull, and manage docker
+            containers anywhere — without tedious environment configuration or
+            management.
+          </p>
+          <div className="my-5">
+            <button
+              id="build-button"
+              className={`group relative h-12 w-48 overflow-hidden rounded-2xl ${
+                selectedButton === "deploy" ? "bg-blue-500" : "bg-gray-500"
+              } text-lg font-bold text-white mx-5`}
+              onClick={() => handleButtonClick("deploy")}
+            >
+              Deploy
+              <div className="absolute inset-0 h-full w-full scale-0 rounded-2xl transition-all duration-300 group-hover:scale-100 group-hover:bg-white/30"></div>
+            </button>
+
+            <button
+              id="deploy-button"
+              className={`group relative h-12 w-48 overflow-hidden rounded-2xl ${
+                selectedButton === "pull" ? "bg-blue-500" : "bg-gray-500"
+              } text-lg font-bold text-white`}
+              onClick={() => handleButtonClick("pull")}
+            >
+              Pull
+              <div className="absolute inset-0 h-full w-full scale-0 rounded-2xl transition-all duration-300 group-hover:scale-100 group-hover:bg-white/30"></div>
+            </button>
+
+            <button
+              id="manage-button"
+              className={`group relative h-12 w-48 overflow-hidden rounded-2xl ${
+                selectedButton === "manage" ? "bg-blue-500" : "bg-gray-500"
+              } text-lg font-bold text-white mx-5`}
+              onClick={() => handleButtonClick("manage")}
+            >
+              Manage
+              <div className="absolute inset-0 h-full w-full scale-0 rounded-2xl transition-all duration-300 group-hover:scale-100 group-hover:bg-white/30"></div>
+            </button>
+          </div>
+
+          {selectedButton === "deploy" && (
+            <div className="flex items-center justify-between container my-10">
+              <div className="text-left text-gray-500 mx-10">
+                <h2 className="mb-4 text-4xl font-extrabold text-gray-900 dark:text-white text-left">
+                  Deploy
+                </h2>
+                <div>
+                  <h5 className="font-bold text-lg text-gray-400">
+                    Deploy and Push Images
+                  </h5>
+                  <p className="my-3">
+                    Efficiently deploy Docker images to Docker Hub, accelerating
+                    your development lifecycle.
+                  </p>
+                </div>
+              </div>
+              <div className="">
+                <img src={push} alt="push" className="h-96" />
+              </div>
+            </div>
+          )}
+
+          {selectedButton === "pull" && (
+            <div className="flex items-center justify-between container my-10">
+              <div className="text-left text-gray-500 mx-10">
+                <h2 className="mb-4 text-4xl font-extrabold text-gray-900 dark:text-white text-left">
+                  Pull
+                </h2>
+                <div>
+                  <h5 className="font-bold text-lg text-gray-400">
+                    Pull Docker Images
+                  </h5>
+                  <p className="my-3">
+                    Effortlessly fetch Docker images from Docker Hub with ease,
+                    ensuring seamless integration into your projects.
+                  </p>
+                </div>
+              </div>
+              <div className="">
+                <img src={pull} alt="pull" />
+              </div>
+            </div>
+          )}
+
+          {selectedButton === "manage" && (
+            <div className="flex items-center justify-between container my-10">
+              <div className="text-left text-gray-500 mx-10">
+                <h2 className="mb-4 text-4xl font-extrabold text-gray-900 dark:text-white text-left">
+                  Manage
+                </h2>
+                <div>
+                  <h5 className="font-bold text-lg text-gray-400">
+                    Manage Containers
+                  </h5>
+                  <p className="my-3">
+                    Gain full control over containerized applications
+                  </p>
+                  <p className="my-3">
+                    Easily view container details, delete containers when
+                    necessary, and manage container lifecycle with options to
+                    stop, start, and more, empowering you to maintain optimal
+                    performance.
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-col justify-between gap-5">
+                <img src={dash} alt="dashboard" />
+                <img src={view} alt="view" />
+              </div>
+            </div>
+          )}
+        </div>
+      </section>
+
+      <section className="bg-gray-50 dark:bg-gray-900" id="about">
         <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
           <div className="grid grid-cols-2 gap-4 mt-8">
             <img
@@ -130,30 +253,19 @@ export const LandingPage = () => {
               a small team, coupled with the scalability and reliability you
               need for your projects.
             </p>
-            <p>
-              Discover the power of DockerWave: where innovation meets
-              efficiency.
-            </p>
           </div>
         </div>
       </section>
 
-      <footer className="p-4 bg-white dark:bg-gray-900">
+      <footer className="p-4 bg-white dark:bg-gray-800">
         <div className="mx-auto max-w-screen-xl">
           <div className="md:flex md:justify-between">
-            <div className="mb-6 md:mb-0">
-              <a href="https://flowbite.com" className="flex items-center">
-                <svg
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="fill-current text-white h-8 w-8 mr-2"
-                >
-                  <path d="M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z" />
-                </svg>
-                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                  DockerWave
-                </span>
-              </a>
+            <div className="mb-6 md:mb-0 flex items-center">
+              <img src={wave} alt="wave" className="h-20 w-20 mr-2" />
+
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                DockerWave
+              </span>
             </div>
             <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
               <div>
@@ -208,7 +320,10 @@ export const LandingPage = () => {
             </div>
           </div>
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-          <div className="sm:flex sm:items-center sm:justify-between">
+          <div
+            className="sm:flex sm:items-center sm:justify-between"
+            id="contact"
+          >
             <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
               © 2024 DockerWave. All Rights Reserved.
             </span>
@@ -216,36 +331,6 @@ export const LandingPage = () => {
               className="flex mt-4 space-x-6 sm:justify-center sm:mt-0"
               id="contact"
             >
-              <a
-                href="#"
-                className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
-              >
-                <svg
-                  className="w-5 h-5"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </a>
-              <a
-                href="#"
-                className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
-              >
-                <svg
-                  className="w-5 h-5"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  aria-hidden="true"
-                >
-                  <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-                </svg>
-              </a>
               <a
                 href="https://github.com/Cyril26"
                 className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
